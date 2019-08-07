7f4244e2f82d1d9b770876c168d38a4a331ec4cf
Tomasz Świątek <tomasz.swiatek@enova.pl>
2019-08-07 13:16:56 +0200

    Poprawa błędów związanych z brakiem lub niepoprawnym generowaniem końcówek nazw (.UI, .Tests) folderów projeków

--- Soneta.Platform.Developer/ItemTemplateWizard/Wizards/SonetaAddonProjectTemplateWizard.cs
+++ Soneta.Platform.Developer/ItemTemplateWizard/Wizards/SonetaAddonProjectTemplateWizard.cs
@@ -11,10 +11,9 @@ namespace ItemTemplateWizard.Wizards
     {
         private DTE dte;
         private string solutionDir;
-        private string projectDir;
+        private string destinationDir;
         private string projectSuffix;
-        private bool configProject;
-        const string solutionItemsFolderName = "Solution Items";
+        private const string SolutionItemsFolderName = "Solution Items";
 
         public void BeforeOpeningFile(ProjectItem projectItem)
         {
@@ -22,28 +21,10 @@ namespace ItemTemplateWizard.Wizards
 
         public void ProjectFinishedGenerating(Project project)
         {
-            project.Name = Path.ChangeExtension(project.Name, projectSuffix);
-
-            if (configProject)
-            {
-                var solutionItemsFolder = dte.Solution.Projects.Cast<Project>().FirstOrDefault(x => x.Name.Equals(solutionItemsFolderName, System.StringComparison.OrdinalIgnoreCase));
-                solutionItemsFolder = solutionItemsFolder ?? ((Solution2)dte.Solution).AddSolutionFolder(solutionItemsFolderName);
-
-                foreach (ProjectItem item in project.ProjectItems)
-                {
-                    var sourcePath = Path.Combine(projectDir, item.Name);
-                    var targetPath = Path.Combine(solutionDir, item.Name);
-                    File.Move(sourcePath, targetPath);
-                    solutionItemsFolder.ProjectItems.AddFromFile(targetPath);
-                }
-
-                dte.Solution.Remove(project);
-
-                if (projectDir == solutionDir)
-                    File.Delete(project.FullName);
-                else
-                    Directory.Delete(projectDir, true);
-            }
+            if (projectSuffix == "Config")
+                ProcessConfigFiles(project);
+            else
+                EnsureSuffix(project);
         }
 
         public void ProjectItemFinishedGenerating(ProjectItem projectItem)
@@ -58,14 +39,74 @@ namespace ItemTemplateWizard.Wizards
         {
             dte = (DTE)automationObject;
             replacementsDictionary.TryGetValue("$solutiondirectory$", out solutionDir);
-            replacementsDictionary.TryGetValue("$destinationdirectory$", out projectDir);
+            replacementsDictionary.TryGetValue("$destinationdirectory$", out destinationDir);
             replacementsDictionary.TryGetValue("$projectsuffix$", out projectSuffix);
-            configProject = projectSuffix == "Config";
         }
 
         public bool ShouldAddProjectItem(string filePath)
         {
             return true;
-        } 
+        }
+
+        private void ProcessConfigFiles(Project project)
+        {
+            var solutionItemsFolder = GetProjects(SolutionItemsFolderName).FirstOrDefault() ?? ((Solution2)dte.Solution).AddSolutionFolder(SolutionItemsFolderName);
+
+            foreach (ProjectItem item in project.ProjectItems)
+            {
+                var sourcePath = Path.Combine(destinationDir, item.Name);
+                var targetPath = Path.Combine(solutionDir, item.Name);
+                File.Move(sourcePath, targetPath);
+                solutionItemsFolder.ProjectItems.AddFromFile(targetPath);
+            }
+
+            dte.Solution.Remove(project);
+
+            if (destinationDir == solutionDir)
+                File.Delete(project.FullName);
+            else
+                Directory.Delete(destinationDir, true);
+        }
+
+        private void EnsureSuffix(Project project)
+        {
+            if (string.IsNullOrEmpty(projectSuffix))
+                return;
+
+            project.Name = GetProjectNameWithSuffix(project);
+            EnsureProjectDirectoryNameWithSuffix(project);
+        }
+
+        private string GetProjectNameWithSuffix(Project project)
+        {
+            var sourceExtension = Path.GetExtension(project.Name);
+            var targetExtension = $".{projectSuffix}";
+
+            if (sourceExtension.Equals(targetExtension, System.StringComparison.InvariantCultureIgnoreCase))
+                return Path.ChangeExtension(project.Name, targetExtension);
+            else
+                return project.Name + targetExtension;
+        }
+
+        private void EnsureProjectDirectoryNameWithSuffix(Project project)
+        {
+            if (destinationDir == solutionDir || project.Name == Path.GetFileName(destinationDir))
+                return;
+
+            dte.Solution.Remove(project);
+            var targetDir = Path.Combine(solutionDir, project.Name);
+            Directory.Move(destinationDir, targetDir);
+            dte.Solution.AddFromFile(Path.Combine(targetDir, Path.GetFileName(project.FullName)));
+        }
+
+        private IEnumerable<Project> GetProjects(string name = null)
+        {
+            var projects = dte.Solution.Projects.Cast<Project>();
+
+            if (!string.IsNullOrEmpty(name))
+                projects = projects.Where(x => x.Name.Equals(name, System.StringComparison.OrdinalIgnoreCase));
+
+            return projects;
+        }
     }
 }
