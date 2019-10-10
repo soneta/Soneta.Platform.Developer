070109509e3882a02c420ca62cad4270d7bc9b49
swiato <swiatomash@gmail.com>
2019-10-10 15:50:41 +0200

    Na pageformie parametrów worker'a data type powinien wskazywać na klasę parametrów worker'a, a nie klasę worker'a (#16)
    * Na pageformie parametrów worker'a data type powinien wskazywać na klasę parametrów worker'a, a nie klasę worker'a
    
    * Jawne wskazanie parametrów 'Name' i 'CaptionHtml', jako elementów należących do okna parametrów worker'a - porządki w nazwach parametrów
    
    * zmiana kodowania plików template.json na utf-8

--- Soneta.Platform.Developer/ItemTemplateWizard/UI/WorkerWizardForm.Designer.cs
+++ Soneta.Platform.Developer/ItemTemplateWizard/UI/WorkerWizardForm.Designer.cs
@@ -120,7 +120,7 @@
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(142, 13);
             this.label1.TabIndex = 1;
-            this.label1.Text = "Tytuł zakładki (CaptionHtml)";
+            this.label1.Text = "Tytuł okna param. (CaptionHtml)";
             // 
             // label3
             // 
